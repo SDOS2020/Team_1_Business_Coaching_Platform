551c80c913f09ec4a0109fc19350393e3a5f49b0
Anmol Singhal <anmol17332@iiitd.ac.in>
2020-11-04 18:54:28 +0530

    Changes in urls.py

--- Business_Coaching_Platform/Business_Coaching_Platform/urls.py
+++ Business_Coaching_Platform/Business_Coaching_Platform/urls.py
@@ -7,17 +7,11 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('home.urls')),
-<<<<<<< HEAD
     path('', include('user.urls')),
     path('api/', include('enrolment.urls')),
-=======
-    path('register-coach', user_views.registerCoach, name='registerCoach'),
-    path('register-coachee', user_views.registerCoachee, name='registerCoachee'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
-    path('find-coach/', enrolment_views.findCoach, name='findCoach'),
     path('', include('user.urls')),
->>>>>>> 0550754ad18925d1d6a92aced93703aa5ec09a2c
     path('admin/', admin.site.urls),
 ]
