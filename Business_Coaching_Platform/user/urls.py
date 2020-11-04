793d9d047c69e4ddfb1ae8db506199eb10c7a34c
shreyansh7499 <shreyansh17109@iiitd.ac.in>
2020-11-04 23:31:15 +0530

    path names changed according to coding convention

--- Business_Coaching_Platform/user/urls.py
+++ Business_Coaching_Platform/user/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('register-coach/', views.CoachRegisterView.as_view(), name='register_coach'),
     path('update_coach/<int:pk>/', views.CoachUpdateView.as_view(), name='update_coach'),
     path('update_coachee/<int:pk>/', views.CoacheeUpdateView.as_view(), name='update_coachee'),
-    path('register-coachee/', views.CoacheeRegisterView.as_view(), name='register_coachee'),
+    path('register_coachee/', views.CoacheeRegisterView.as_view(), name='register_coachee'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
