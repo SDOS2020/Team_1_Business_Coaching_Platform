669499359d83770976e1d415bbaa8dfca76c7afa
yatin17124 <yatin17124@iiitd.ac.in>
2020-11-24 03:36:12 +0530

    Removed Router Line

--- Business_Coaching_Platform/dashboard/urls.py
+++ Business_Coaching_Platform/dashboard/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path, include
-import user.views as user_views
 import dashboard.views as dashboard_views
 from django.contrib.auth import views as auth_views
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register('connection', user_views.ConnectionViewSet, basename='connection')
 
 urlpatterns = [
     path('dashboard', dashboard_views.dashboard, name='dashboard'),
