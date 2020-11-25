e51d058a52e15a9f0726f9413728c366a2b07a1e
tejasoberoi27 <tejas17367@iiitd.ac.in>
2020-11-25 19:29:35 +0530

    Update Database NAME path to make Windows compatible

--- Business_Coaching_Platform/Business_Coaching_Platform/settings.py
+++ Business_Coaching_Platform/Business_Coaching_Platform/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'Business_Coaching_Platform.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
