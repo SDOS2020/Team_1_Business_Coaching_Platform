0bc5bb9131ff04a53036f067fb36bb563b171433
tejasoberoi27 <tejas17367@iiitd.ac.in>
2020-12-03 18:19:29 +0530

    include dedicated_page in installed apps

---
+++ Business_Coaching_Platform/Business_Coaching_Platform/asgi.py
@@ -0,0 +1,29 @@
+"""
+ASGI config for Business_Coaching_Platform project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter
+import notification.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Business_Coaching_Platform.settings')
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                notification.routing.websocket_urlpatterns
+                )
+            ),
+    ),
+})
