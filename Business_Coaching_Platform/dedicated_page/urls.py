5e7f8b3a188f054c803dc5b26521067dff2d3ee5
tejasoberoi27 <tejas17367@iiitd.ac.in>
2020-12-08 00:05:10 +0530

    improve css of dedicated_page

--- Business_Coaching_Platform/dedicated_page/urls.py
+++ Business_Coaching_Platform/dedicated_page/urls.py
@@ -10,11 +10,8 @@ router = DefaultRouter()
 router.register('post', PostViewSet, basename='post')
 
 urlpatterns = [
-    # path('viewset/', include(router.urls))
     path('api/', include(router.urls)),
     path('api/<int:pk>/', include(router.urls)),
-    # path('post/',TemplateView.as_view(template_name='page.html')),
-    # path('post/new/<int:pk>',dedicated_page_views.create_post, name='post_create'),
     url(r'^dedicated_page/(?P<pk>\d+)/$', dedicated_page_views.dedicated_page, name='dedicated_page'),
 
 ]
