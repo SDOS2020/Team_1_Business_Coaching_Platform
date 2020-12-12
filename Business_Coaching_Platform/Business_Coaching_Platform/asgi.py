353b3b99c5d6d95906362ac3d5f54b62e7d1075c
Anmol Singhal <anmol17332@iiitd.ac.in>
2020-12-13 03:27:44 +0530

    settings

--- Business_Coaching_Platform/Business_Coaching_Platform/asgi.py
+++ Business_Coaching_Platform/Business_Coaching_Platform/asgi.py
@@ -10,15 +10,17 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Business_Coaching_Platform.settings")
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 import notification.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Business_Coaching_Platform.settings')
-
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
+  "http": django_asgi_app,
   "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
