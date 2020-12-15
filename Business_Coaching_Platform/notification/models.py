973cc8fe43edc7383644ab2c79e986d3cf3787d2
shreyansh7499 <shreyansh17109@iiitd.ac.in>
2020-12-15 22:56:53 +0530

    Add Notification

--- Business_Coaching_Platform/notification/models.py
+++ Business_Coaching_Platform/notification/models.py
@@ -2,5 +2,6 @@ from django.db import models
 from user.models import CustomUser
 
 class Notification(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
+    sender = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name='receiver')
     event = models.CharField(max_length = 50)
--- Business_Coaching_Platform/notification/urls.py
+++ Business_Coaching_Platform/notification/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+
+router = DefaultRouter()
+router.register('notification', views.NotificationViewSet, basename='notification')
+
+
 urlpatterns = [
-    path('<str:room_name>/', views.room, name='room'),
+    path('api/', include(router.urls)),
+    path('api/<int:pk>/', include(router.urls)),
 ]
