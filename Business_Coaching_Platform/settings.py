0fb1243add52667c8ebd4b1f546fbf6657731e0c
shreyansh7499 <shreyansh17109@iiitd.ac.in>
2020-12-20 05:06:20 +0530

    cors

--- Business_Coaching_Platform/settings.py
+++ Business_Coaching_Platform/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsPostCsrfMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -68,7 +69,6 @@ MIDDLEWARE = [
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
-
 
 
 CORS_ALLOW_METHODS = list(default_methods)
