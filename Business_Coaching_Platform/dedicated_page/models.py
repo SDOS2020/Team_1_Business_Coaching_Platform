311c1fb438f012c5140937b801c11e9eac19167d
tejasoberoi27 <tejas17367@iiitd.ac.in>
2020-12-19 20:01:43 +0530

    Use bootstrap for post

--- Business_Coaching_Platform/dedicated_page/models.py
+++ Business_Coaching_Platform/dedicated_page/models.py
@@ -4,8 +4,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.conf import settings
 import os
-# print("PATH",os.getcwd())
-# noinspection PyUnresolvedReferences
+
 from user.models import CustomUser,Coach, Coachee
 
 
@@ -15,9 +14,9 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     creator = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name='post_creator')
     viewer = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name='post_viewer')
-    # post_file = models.FileField(null = True, blank = True, upload_to = 'post_files')
     uploaded_file_url = models.CharField(null=True, max_length=500)
     uploaded_file_name = models.CharField(null=True, max_length=500)
+
     def __str__(self):
         return f"{self.creator} -> {self.viewer} : {self.content}"
 
