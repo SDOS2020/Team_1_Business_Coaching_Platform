eb1c4a42c59755abb8625b72f1baf28b9c794fe8
tejasoberoi27 <tejas17367@iiitd.ac.in>
2020-12-20 23:16:42 +0530

    added tests dir and delete title from model Post in dedicated_page app

--- Business_Coaching_Platform/dedicated_page/models.py
+++ Business_Coaching_Platform/dedicated_page/models.py
@@ -9,7 +9,6 @@ from user.models import CustomUser,Coach, Coachee
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     creator = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name='post_creator')
---
+++ Business_Coaching_Platform/dedicated_page/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from dedicated_page.models import Post
+
+
+# Create your tests here.
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Post.objects.create()
+        pass
+
+    def setUp(self):
+        print("setUp: Run once for every test method to setup clean data.")
+        pass
+
+    def test_false_is_false(self):
+        print("Method: test_false_is_false.")
+        self.assertFalse(False)
+
+    def test_false_is_true(self):
+        print("Method: test_false_is_true.")
+        self.assertTrue(False)
+
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
