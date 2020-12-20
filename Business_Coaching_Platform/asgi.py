f192a6629a9eed88f7ca7027738acd37aaf36955
shreyansh7499 <shreyansh17109@iiitd.ac.in>
2020-12-21 01:37:05 +0530

    update asgi to include authentication

--- Business_Coaching_Platform/asgi.py
+++ Business_Coaching_Platform/asgi.py
@@ -14,16 +14,18 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Business_Coaching_Platform.settings")
 django_asgi_app = get_asgi_application()
 
-
+from django.urls import re_path
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 from notification.consumers import NotificationConsumer
 
 application = ProtocolTypeRouter({
-  "http": django_asgi_app,
-  "websocket": 
-       URLRouter([
-            url(r'ws/notification/$', NotificationConsumer.as_asgi()),
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            re_path(r"^ws/notification/$", NotificationConsumer.as_asgi()),
         ])
+    ),
+
 })
